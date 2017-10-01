9a0e5fc862a98f89d943fae2df1240cf91a75382
Red <redoine@minatjanster.se>
2017-10-01 23:20:46 +0200

    Basic views for Home and creating a match

---
+++ mobile-app/EasyRank/app/components/Home/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View} from 'react-native';
+
+export default class Home extends Component {
+  render() {
+    return (
+      <View >
+        Hello there !
+      </View>
+    )
+  }
+};
