de9aa3c3b463ed3ed3cbdde1a592422141d14209
siMs0n <simon.c.nielsen@gmail.com>
2017-10-17 23:45:00 +0200

    Added more player images

--- mobile-app/EasyRank/app/models/players.js
+++ mobile-app/EasyRank/app/models/players.js
@@ -4,7 +4,10 @@ export const playerAvatars = {
   "simon": require('../assets/images/avatars/simon.png'),
   "red": require('../assets/images/avatars/red.png'),
   "clobbe": require('../assets/images/avatars/clobbe.png'),
-  "fredrik": require('../assets/images/avatars/fredrik.png')
+  "fredrik": require('../assets/images/avatars/fredrik.png'),
+  "linus": require('../assets/images/avatars/linus.png'),
+  "stoffe": require('../assets/images/avatars/stoffe.png'),
+  "joakim": require('../assets/images/avatars/joakim.png')
 };
 
 export const playerNameToAvatarImageSource = (playerName) => {
